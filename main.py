bdf3310ae53cfd68017706f5ed923e10786079af
Nintechblox <ultragamerxd2016@gmail.com>
2026-02-13 18:43:52 -0500

    added some fun stuff!

--- main.py
+++ main.py
@@ -1,17 +1,38 @@
 from openai import OpenAI
 import os
 
+# Hugging Face API, not tested.
 client = OpenAI(base_url='https://router.huggingface.co/v1', api_key=os.environ["HF_TOKEN"])
 
 # Gets user prediction request.
 userinput = input("I am the magic 8 ball, ask me something, and I will predict your future!  ")
 
+# Asking for some fun extras :)
+wackymode = input("Do you want it to be extremely wacky? Yes or No only, capital letter first only.  ")
+freedommode = input("Do you want the built in options for the answers, or for the AI to make them up? Say Freedom to turn on freedom mode, capital only.   " )
+
+# Determining if to add extra's
+if wackymode == "Yes":
+    wackysysprompttext = "Please be EXTREMELY WACKY, DO NOT BE NORMAL, MAKE THE PREDICTIONS WACKY!!"
+else:
+    print("I guess you don't want it to be wacky, OK!")
+    wackysysprompttext = "Do not be wacky."
+
+if freedommode == "Freedom":
+    freedomsysprompttext = "You can make up any option you want, no matter what. ONE, TWO OR THREE WORDS ONLY"
+else:
+    print("I guess you don't want it to make up it's own predictions, OK!")
+    freedomsysprompttext = "you can only respond with Yes, No, Maybe, Definetly Not, Less Likely, Unsure, and 50/50."
+
+
 # Sends data to API along with magic 8 ball system message.
 
+# No if statements to put in wacky and freedom mode, all in one.
 response = client.chat.completions.create(
     model="deepseek-ai/DeepSeek-V3-0324",
-    messages=[{"role": "user", "content": f"You are a magic 8 ball that makes predictions for fun, you can only respond with Yes, No, Maybe, Definetly Not, Less Likely, Unsure, and 50/50. The prediction to answer (choose a bit accuratly like this is going to be real) is: {userinput} DO NOT PUT IN ANY EMOJIS!! THIS IS FOR FUN ONLY!!!"}]
+    messages=[{"role": "user", "content": f"You are a magic 8 ball that makes predictions for fun, {freedomsysprompttext} The prediction to answer (choose a bit accuratly like this is going to be real) is: {userinput} DO NOT PUT IN ANY EMOJIS!! THIS IS FOR FUN ONLY!!! {wackysysprompttext}"}]
 )
+
 
 # Displays prediction results
 print(f"\n \n PREDITCTION RESULTS: {response.choices[0].message.content}")
