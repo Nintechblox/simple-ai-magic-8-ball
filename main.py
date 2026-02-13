c7817ab9ae2b0648df09a39b90b799c19ee28a1e
Nintechblox <ultragamerxd2016@gmail.com>
2026-02-13 17:22:18 -0500

    the base

---
+++ main.py
@@ -0,0 +1,17 @@
+from openai import OpenAI
+client = OpenAI()
+
+# Gets user prediction request.
+userinput = input("I am the magic 8 ball, ask me something, and I will predict your future!  ")
+
+# Sends data to API along with magic 8 ball system message.
+response = client.responses.create(
+    model="chatgpt-4o-latest",
+    input=f"You are a magic 8 ball that makes predictions for fun, you can only respond with Yes, No, Maybe, Definetly Not, Less Likely, Unsure, and 50/50. The prediction to answer (choose a bit accuratly like this is going to be real) is: {userinput} DO NOT PUT IN ANY EMOJIS"
+)
+
+# Displays prediction results
+print(f"\n \n PREDITCTION RESULTS: {response.output_text}")
+
+# For anyone thinking this is AI written, this is all human made, sorry for AI looking comments lol
+# - Nintechblox XD
